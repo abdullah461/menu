cccceef468c2583118dcc84fde04eb6c17bcd78f
abdull461 <abdullahabubakar461@gmail.com>
2023-05-17 12:50:52 +0100

    setting up sqlalchemy

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from os import path
+from flask_sqlalchemy import SQLAlchemy
 
 
+# create extention
+db = SQLAlchemy()
 
+# create app
 def create_app():
     app = Flask(__name__)
 
+    # secret key
+    app.config['SECRET_KEY'] = 'abdullahiabubakar'
+
+    # configure the SQLite database, relative to the app instance folder
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+    # initialize the app
+    db.init_app(app)
 
     from .views import views
 
